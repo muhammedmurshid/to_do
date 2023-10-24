6310ece194d6e2f75294ef1530e6d76eecf322ff
Murshid <muhsink00000@gmail.com>
2023-10-24 16:16:31 +0530

    added completed date

--- models/to_do.py
+++ models/to_do.py
@@ -31,6 +31,7 @@ class ToDoTasks(models.Model):
     assigned_date = fields.Date(string='Assigned Date', default=date.today())
     assigned_to = fields.Many2one('res.users', string='Assigned To', domain=[('faculty_check', '=', False)],
                                   readonly=False, required=True, )
+    completed_date = fields.Date(string='Completed Date')
     task_organizer = fields.Many2one('res.users', string='Task Organizer', default=lambda self: self.env.user)
 
     def _compute_get_employee(self):
@@ -137,6 +138,7 @@ class ToDoTasks(models.Model):
         other_activity_ids = self.env['mail.activity'].search([('res_id', '=', self.id), (
             'activity_type_id', '=', self.env.ref('to_do.activity_to_do_activity_custom').id)])
         other_activity_ids.unlink()
+        self.completed_date = datetime.now()
         self.write({'state': 'completed'})
 
     def action_cancel(self):
