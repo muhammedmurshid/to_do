9a8d3934a93a243d2a2de68f52e19a9716747c6c
muhammedmurshid <muhsink00000@gmail.com>
2024-12-21 17:11:24 +0530

    added patch management

--- models/project.py
+++ models/project.py
@@ -16,9 +16,41 @@ class ProjectAddFields(models.Model):
     status = fields.Selection([('new_request', 'New Request'), ('testing', 'Testing'), ('code_review', 'Code Review'),
                                ('approval_pending', 'Approval Pending'),
                                ('ready_for_production', 'Ready for Production'), ('production', 'Production'),
-                               ('closed', 'Closed'), ('rejected', 'Rejected')], tracking=1, string="Status", default='new_request')
+                               ('closed', 'Closed'), ('rejected', 'Rejected')], tracking=1, string="Status",
+                              default='new_request')
     approver_id = fields.Many2one('res.users', string="Approver",
                                   default=lambda self: self.env.user.employee_id.parent_id.user_id)
+    project_type = fields.Selection(
+        [('changing_management', 'Changing Management'), ('patching_management', 'Patching Management')],
+        string="Project Type", )
+    server_name = fields.Selection([('odoo_server', 'Odoo Server'), ('punching_server', 'Punching Server')],
+                                   string="Server Name")
+    server_id = fields.Char(string="Server ID")
+    version_from = fields.Char(string="Version (From)")
+    version_to = fields.Char(string="Version (To)")
+    patch_name = fields.Char(string="Patch Name")
+    start_date = fields.Date(string="Start Date")
+    end_date = fields.Date(string="End Date")
+    comments = fields.Text(string="Comments")
+    patching_status = fields.Selection(
+        [('request_logged', 'Request Logged'), ('pending_approval', 'Pending Approval'), ('approved', 'Approved'), ('rejected', 'Rejected'),
+         ('deployment_in_progress', 'Deployment In progress'), ('completed', 'Completed')], string="Patch Status")
+
+    @api.onchange('project_type')
+    def _onchange_project_type(self):
+        if self.project_type == 'changing_management':
+            self.server_name = False
+            self.server_id = False
+            self.version_to = False
+            self.version_from = False
+            self.patch_name = False
+            self.start_date = False
+            self.end_date = False
+            self.comments = False
+        if self.project_type == 'patching_management':
+            self.testing_summary = False
+            self.code_review_notes = False
+            self.change_description = False
 
     def action_sent_to_test(self):
         stage = self.env['project.task.type'].sudo().search([('name', '=', 'Testing')], limit=1)
@@ -49,6 +81,7 @@ class ProjectAddFields(models.Model):
                 raise ValidationError(_('Only the Head of Department can approve this action.'))
         else:
             raise ValidationError(_('Please Add Approver.'))
+
     def action_production(self):
         stage = self.env['project.task.type'].sudo().search([('name', '=', 'Production')], limit=1)
         if stage:
@@ -72,3 +105,46 @@ class ProjectAddFields(models.Model):
                 raise ValidationError(_('Only the Head of Department can reject this action.'))
         else:
             raise ValidationError(_('Please Add Approver.'))
+
+    def action_patch_sent_to_approve(self):
+        stage = self.env['project.task.type'].sudo().search([('name', '=', 'Pending Approval')], limit=1)
+        if stage:
+            self.stage_id = stage.id
+        self.patching_status = 'pending_approval'
+
+    def action_head_approve(self):
+        if self.approver_id:
+            if self.env.user.id == self.approver_id.id:
+                stage = self.env['project.task.type'].sudo().search([('name', '=', 'Approved')], limit=1)
+                if stage:
+                    self.stage_id = stage.id
+                self.patching_status = 'approved'
+            else:
+                raise ValidationError(_('Only the Head of Department can approve this action.'))
+
+        else:
+            raise ValidationError(_('Please Add Approver.'))
+
+    def action_head_refuse(self):
+        if self.approver_id:
+            if self.env.user.id == self.approver_id.id:
+                stage = self.env['project.task.type'].sudo().search([('name', '=', 'Rejected')], limit=1)
+                if stage:
+                    self.stage_id = stage.id
+                self.patching_status = 'rejected'
+            else:
+                raise ValidationError(_('Only the Head of Department can reject this action.'))
+        else:
+            raise ValidationError(_('Please Add Approver.'))
+
+    def action_deployment_in_progress(self):
+        stage = self.env['project.task.type'].sudo().search([('name', '=', 'Deployment in Progress')], limit=1)
+        if stage:
+            self.stage_id = stage.id
+        self.patching_status = 'deployment_in_progress'
+
+    def action_completed(self):
+        stage = self.env['project.task.type'].sudo().search([('name', '=', 'Completed')], limit=1)
+        if stage:
+            self.stage_id = stage.id
+        self.patching_status = 'completed'
